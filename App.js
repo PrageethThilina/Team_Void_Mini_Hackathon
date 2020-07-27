e3a0944a256fbda56a6d7ce1268be590a80ad009
sajana96 <weerakonsajana@gmail.com>
2020-07-27 11:50:33 +0530

    Dashboard Completed

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Spinner } from 'native-base'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
 import { navigationRef } from './RootNavigation'
 import Login from './screens/Login'
 import Dashboard from './screens/Dashboard'
 
 const Stack = createStackNavigator()
 const App = () => {
+  const [ready, setReady] = useState(false)
+  useEffect(async () => {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    })
+    setReady(true)
+  }, [])
+  if (!ready) {
+    return <Spinner color='red' />
+  }
   return (
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Dashboard' component={Dashboard} />
+        <Stack.Screen
+          name='Dashboard'
+          component={Dashboard}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,94 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 
-const Chat = ({ route, navigation }) => {
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Picker,
+  Spinner,
+  List,
+  ListItem,
+} from 'native-base'
+
+const Dashboard = ({ route, navigation }) => {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <Container>
+      <Header style={{ backgroundColor: '#cf0404' }}>
+        <Body>
+          <Title>Dashboard</Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon name='search' />
+          </Button>
+
+          <Button transparent>
+            <Icon name='person' />
+          </Button>
+        </Right>
+      </Header>
+      <Text
+        style={{
+          fontSize: 40,
+          textAlign: 'center',
+          fontWeight: 'bold',
+        }}
+      >
+        Search for a Gift...
+      </Text>
+      <Content style={{ marginTop: 40 }}>
+        <List>
+          <ListItem>
+            <Body>
+              <Text style={{ fontSize: 25 }}>Previous Searches</Text>
+            </Body>
+
+            <Right>
+              <Button style={{ backgroundColor: '#cf0404' }}>
+                <Icon active name='arrow-forward' />
+              </Button>
+            </Right>
+          </ListItem>
+          <ListItem>
+            <Body>
+              <Text style={{ fontSize: 25 }}>Most Searched Gifts</Text>
+            </Body>
+
+            <Right>
+              <Button style={{ backgroundColor: '#cf0404' }}>
+                <Icon active name='arrow-forward' />
+              </Button>
+            </Right>
+          </ListItem>
+          <ListItem>
+            <Body>
+              <Text style={{ fontSize: 25 }}>Search gift</Text>
+            </Body>
+
+            <Right>
+              <Button style={{ backgroundColor: '#cf0404' }}>
+                <Icon active name='arrow-forward' />
+              </Button>
+            </Right>
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
   )
 }
 
-export default Chat
+export default Dashboard
